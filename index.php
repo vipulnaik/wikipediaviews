5f94b20dd239fc4302e788455e4bd8e75c263a68
Vipul Naik <vipulnaik1@gmail.com>
2016-11-19 05:54:19 +0000

    Checkpointing still partial changes

--- index.php
+++ index.php
@@ -3,12 +3,12 @@ print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><hea
 print '<title>Wikipedia Views</title>';
 include("style/toggler.inc"); ##included in all public-facing files
 print '</head>';
-# print '<p>The website is down for maintenance right now</p>';
-include_once("backend/corecode.inc");
-include("style/head.inc"); ##included in all public-facing files
-print '<body>';
-$formdata = false;
-include("inputdisplay/onemonthdataentry.inc");
-print '</body>';
+print '<p>The website is down for maintenance right now</p>';
+# include_once("backend/corecode.inc");
+# include("style/head.inc"); ##included in all public-facing files
+# print '<body>';
+# $formdata = false;
+# include("inputdisplay/onemonthdataentry.inc");
+# print '</body>';
 print '</html>';
 ?>

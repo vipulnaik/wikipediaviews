a03bf98bc85da617f88ce7d24b6a00b4026fc9f7
Vipul Naik <vipulnaik1@gmail.com>
2017-02-02 18:18:55 +0000

    Added delete command to avoid unnecessary computation

--- sql/facebook-data.sql
+++ sql/facebook-data.sql
@@ -1,1 +1,3 @@
 insert ignore into wantedviewcountsbymonth(pagename, monthfull, `language`, drilldown, status) select pagename, '201701', `language`, 'cumulative-facebook-shares', 'wanted' from (select distinct pagename, `language` from viewcountsbymonth) t1;
+
+delete from wantedviewcountsbymonth where (pagename, monthfull, `language`, drilldown) in (select pagename, monthfull, `language`, drilldown from viewcountsbymonth where monthfull = '201701' and drilldown = 'cumulative-facebook-shares');

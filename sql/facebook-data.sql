4c9ae5dd326c358d6e9ae9a83a8bab5a78b193dd
Vipul Naik <vipulnaik1@gmail.com>
2017-06-27 15:05:58 +0000

    Update Facebook data file to use 201706 instead of 201705

--- sql/facebook-data.sql
+++ sql/facebook-data.sql
@@ -1,4 +1,4 @@
-insert ignore into wantedviewcountsbymonth(pagename, monthfull, `language`, drilldown, status) select pagename, '201705', `language`, 'cumulative-facebook-shares', 'wanted' from (select distinct pagename, `language` from viewcountsbymonth) t1;
+insert ignore into wantedviewcountsbymonth(pagename, monthfull, `language`, drilldown, status) select pagename, '201706', `language`, 'cumulative-facebook-shares', 'wanted' from (select distinct pagename, `language` from viewcountsbymonth) t1;
 
-delete from wantedviewcountsbymonth where (pagename, monthfull, `language`, drilldown) in (select pagename, monthfull, `language`, drilldown from viewcountsbymonth where monthfull = '201705' and drilldown = 'cumulative-facebook-shares');
+delete from wantedviewcountsbymonth where (pagename, monthfull, `language`, drilldown) in (select pagename, monthfull, `language`, drilldown from viewcountsbymonth where monthfull = '201706' and drilldown = 'cumulative-facebook-shares');
 

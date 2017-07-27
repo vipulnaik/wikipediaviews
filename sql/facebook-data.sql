55a749ba257c4f4c8fbd0f34cb28c819bcd5afc8
Vipul Naik <vipulnaik1@gmail.com>
2017-07-27 20:03:25 +0000

    Updates for Facebook data

--- sql/facebook-data.sql
+++ sql/facebook-data.sql
@@ -1,4 +1,4 @@
-insert ignore into wantedviewcountsbymonth(pagename, monthfull, `language`, drilldown, status) select pagename, '201706', `language`, 'cumulative-facebook-shares', 'wanted' from (select distinct pagename, `language` from viewcountsbymonth where viewcount > 0) t1;
+insert ignore into wantedviewcountsbymonth(pagename, monthfull, `language`, drilldown, status) select pagename, '201707', `language`, 'cumulative-facebook-shares', 'wanted' from (select distinct pagename, `language` from viewcountsbymonth where drilldown = 'desktop' and viewcount > 5) t1;
 
-delete from wantedviewcountsbymonth where (pagename, monthfull, `language`, drilldown) in (select pagename, monthfull, `language`, drilldown from viewcountsbymonth where monthfull = '201706' and drilldown = 'cumulative-facebook-shares');
+delete from wantedviewcountsbymonth where (pagename, monthfull, `language`, drilldown) in (select pagename, monthfull, `language`, drilldown from viewcountsbymonth where monthfull = '201707' and drilldown = 'cumulative-facebook-shares');
 

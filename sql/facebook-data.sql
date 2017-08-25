d09e7afc7a5bfa8757972f95ea3e1d4e0855612d
Vipul Naik <vipulnaik1@gmail.com>
2017-08-25 14:08:00 +0000

    Update viewcount month and some settings

--- sql/facebook-data.sql
+++ sql/facebook-data.sql
@@ -1,4 +1,4 @@
-insert ignore into wantedviewcountsbymonth(pagename, monthfull, `language`, drilldown, status) select pagename, '201707', `language`, 'cumulative-facebook-shares', 'wanted' from (select distinct pagename, `language` from viewcountsbymonth where drilldown = 'desktop' and viewcount > 5) t1;
+insert ignore into wantedviewcountsbymonth(pagename, monthfull, `language`, drilldown, status) select pagename, '201708', `language`, 'cumulative-facebook-shares', 'wanted' from (select distinct pagename, `language` from viewcountsbymonth where drilldown = 'desktop' and viewcount > 20) t1;
 
-delete from wantedviewcountsbymonth where (pagename, monthfull, `language`, drilldown) in (select pagename, monthfull, `language`, drilldown from viewcountsbymonth where monthfull = '201707' and drilldown = 'cumulative-facebook-shares');
+delete from wantedviewcountsbymonth where (pagename, monthfull, `language`, drilldown) in (select pagename, monthfull, `language`, drilldown from viewcountsbymonth where monthfull = '201708' and drilldown = 'cumulative-facebook-shares');
 

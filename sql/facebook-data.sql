7261717ae041b292f3f6605379b48c4fbbaa6265
Vipul Naik <vipulnaik1@gmail.com>
2017-08-25 14:21:05 +0000

    Exclude [aggregate] from viewcounts

--- sql/facebook-data.sql
+++ sql/facebook-data.sql
@@ -1,4 +1,4 @@
-insert ignore into wantedviewcountsbymonth(pagename, monthfull, `language`, drilldown, status) select pagename, '201708', `language`, 'cumulative-facebook-shares', 'wanted' from (select distinct pagename, `language` from viewcountsbymonth where drilldown = 'desktop' and viewcount > 20) t1;
+insert ignore into wantedviewcountsbymonth(pagename, monthfull, `language`, drilldown, status) select pagename, '201708', `language`, 'cumulative-facebook-shares', 'wanted' from (select distinct pagename, `language` from viewcountsbymonth where drilldown = 'desktop' and pagename != 'aggregate' and viewcount > 20) t1;
 
 delete from wantedviewcountsbymonth where (pagename, monthfull, `language`, drilldown) in (select pagename, monthfull, `language`, drilldown from viewcountsbymonth where monthfull = '201708' and drilldown = 'cumulative-facebook-shares');
 

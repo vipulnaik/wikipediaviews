1dcdd9e358043b1add655912cc9460476faa6be9
Vipul Naik <vipulnaik1@gmail.com>
2020-07-29 19:35:10 +0000

    Add noindex to WV multiple months and multiple years pages so that bots stop indexing those pages

--- displayviewsformultiplemonths.php
+++ displayviewsformultiplemonths.php
@@ -1,5 +1,6 @@
 <!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
 <title>Wikipedia Views: results</title>
+<meta name="robots" content="noindex">    
 <?php
 
 include("style/toggler.inc");
--- displayviewsformultipleyears.php
+++ displayviewsformultipleyears.php
@@ -1,4 +1,5 @@
 <!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+<meta name="robots" content="noindex">
 <title>Wikipedia Views: results</title>
 <?php
 

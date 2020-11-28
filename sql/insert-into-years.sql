96d5acf5d29da42165545612909f9b0e75111f1c
Vipul Naik <vipulnaik1@gmail.com>
2020-11-28 08:42:55 -0800

    Schema updates to allow months to go to end of 2030

---
+++ sql/insert-into-years.sql
@@ -0,0 +1,13 @@
+insert into years(year, status) values
+  ('2021','future'),
+  ('2022','future'),
+  ('2023','future'),
+  ('2024','future'),
+  ('2025','future'),
+  ('2026','future'),
+  ('2027','future'),
+  ('2028','future'),
+  ('2029','future'),
+  ('2030','future');
+
+  
